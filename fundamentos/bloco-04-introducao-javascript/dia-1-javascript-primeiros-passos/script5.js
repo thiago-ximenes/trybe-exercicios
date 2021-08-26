f57a244eb9210e0021ae4a3002f4a3086718ac5f
Thiago Ximenes <thiagoximenes@arch.localdomain>
2021-08-25 22:13:27 -0300

    Adiciona ultimo exercício de fixação

---
+++ fundamentos/bloco-04-introducao-javascript/dia-1-javascript-primeiros-passos/script5.js
@@ -0,0 +1,15 @@
+let isAproved = "lista"
+
+switch (isAproved) {
+  case "aprovada":
+    console.log("Aprovada");
+    break;
+  case "lista":
+    console.log("Lista");
+    break;
+  case "reprovada":
+    console.log("Reprovada");
+    break;
+  default:
+    console.log("Não se aplica")
+}
