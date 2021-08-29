7e6195bee6308b059b44e24fcfc27596b95dd6eb
Thiago Ximenes <thiagoximenes@arch.localdomain>
2021-08-29 12:08:30 -0300

    Termina aula/4.2

--- fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios/script6.js
+++ fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios/script6.js
@@ -66,11 +66,12 @@ for (let item of array) {
   console.log(item / 2);
 }
 
-sortedNumbers = numbers.sort(function(a, b){return a - b});
+sortedNumbers = numbers.sort(function (a, b) {
+  return a - b;
+});
 console.log(sortedNumbers);
 
-
-let anything = [7,2,14,5]
+let anything = [7, 2, 14, 5];
 
 for (let index = 1; index < anything.length; index += 1) {
   for (let secondIndex = 0; secondIndex < anything.length; secondIndex += 1) {
@@ -81,17 +82,35 @@ for (let index = 1; index < anything.length; index += 1) {
     }
   }
 }
-console.log(anything)
+console.log(anything);
 
-let something = [7,2,14,5]
+let something = [7, 2, 14, 5];
 
 for (let index = 1; index < something.length; index += 1) {
   for (let secondIndex = 0; secondIndex < something.length; secondIndex += 1) {
     if (something[index] > something[secondIndex]) {
-      let position = something[index]
+      let position = something[index];
       something[index] = something[secondIndex];
       something[secondIndex] = position;
     }
   }
 }
-console.log(something)
+console.log(something);
+
+numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let newNumber = [];
+
+for (let index = 0; index < numbers.length; index += 1) {
+  for (let secondIndex = 1; secondIndex < numbers.length; secondIndex += 1) {
+     if (index === secondIndex - 1) {
+      let multiplyNumber = numbers[index] * numbers[secondIndex];
+      newNumber.push(multiplyNumber);
+    }
+  }
+  if (index === numbers.length - 1) {
+      multiplyNumber = numbers[index] * 2;
+      newNumber.push(multiplyNumber);
+    }
+}
+
+console.log(newNumber);
