6d21201faa9d78578946aad8ebeac702db4ffee8
Thiago Ximenes <thiagoximenes@arch.localdomain>
2021-08-29 12:53:23 -0300

    Desenha quadrado no console usando for a partir de um valor n

---
+++ fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -0,0 +1,10 @@
+let n = 7;
+let joker = "";
+
+for (let counter = 1; counter <= n; counter += 1) {
+  for (let i = 1; i <= n; i += 1) {
+    joker += "* " + "";
+  }
+  joker += "\n";
+}
+console.log(joker);
