a9bb4d82cb8882bf1828ff5ebc927f32e5413af0
Thiago Ximenes <thiagoximenes@arch.localdomain>
2021-09-02 15:31:52 -0300

    Manipula elemntos html e css com JavaScript

---
+++ fundamentos/bloco-05-javascript-dom-e-seletores/fixacao/script.js
@@ -0,0 +1,4 @@
+document.getElementById("page-title").innerText = "Harry Potter";
+document.getElementById("second-paragraph").innerText = "Segundo parágrafo alterado"
+document.getElementById("subtitle").innerText = 'E o Prisioneiro de Azkaban';
+document.getElementById("paragraph").style. color = 'blue';
