5715fce217be62aa501380107ebb5fabb9abdb05
Thiago Ximenes <thiagoximenes@arch.localdomain>
2021-09-02 16:03:42 -0300

    Utiliza getElementsByClassName para manipular elementos por sua classe

--- fundamentos/bloco-05-javascript-dom-e-seletores/fixacao/script.js
+++ fundamentos/bloco-05-javascript-dom-e-seletores/fixacao/script.js
@@ -1,4 +1,10 @@
 document.getElementById("page-title").innerText = "Harry Potter";
 document.getElementById("second-paragraph").innerText = "Segundo parágrafo alterado"
-document.getElementById("subtitle").innerText = 'E o Prisioneiro de Azkaban';
-document.getElementById("paragraph").style. color = 'blue';
+// document.getElementById("subtitle").innerText = 'E o Prisioneiro de Azkaban';
+document.getElementById("paragraph").style.color = 'blue';
+
+p = document.getElementsByClassName("p");
+
+p[0].style.fontSize = "100px"
+
+document.getElementsByClassName("subtitle")[0].style.color = 'green';
