3c88b9c5d2914c866074452f2a8ca46c51e9c918
Thiago Ximenes <thiagoximenes@arch.localdomain>
2021-09-08 15:21:47 -0300

    Upa último exercício feito

--- fundamentos/bloco-05-javascript-dom-e-seletores/exercises/script.js
+++ fundamentos/bloco-05-javascript-dom-e-seletores/exercises/script.js
@@ -7,14 +7,16 @@ document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor =
   "#F9DB5E";
 
 let leftSide = document.querySelectorAll(".emergency-tasks h3");
-
-for (let index = 0; index < leftSide.length; index += 1) {
-  leftSide[index].style.backgroundColor = "#A500F3";
+// for (let index = 0; index < leftSide.length; index += 1) {
+  for (let item of leftSide){
+    item.style.backgroundColor = "#A500F3";
+  // leftSide[index].style.backgroundColor = "#A500F3";
 }
 
 let rightSide = document.querySelectorAll(".no-emergency-tasks h3");
-for (let index = 0; index < rightSide.length; index += 1) {
-  rightSide[index].style.backgroundColor = "#232525";
-}
+// for (let index = 0; index < rightSide.length; index += 1) {
+  for (let item of rightSide) {
+    item.style.backgroundColor = "#232525";
+  }
 
 document.getElementById("footer-container").style.backgroundColor = '#003533';
